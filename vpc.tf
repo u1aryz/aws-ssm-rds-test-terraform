6dd93dd59aebd4ccae949d6243d07ed10a1da2b9
u1aryz <u1aryz.d@gmail.com>
2023-10-04 14:18:22 +0900

    Add app_name variable and use it in vpc module name

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ data "aws_availability_zones" "available" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
-  name    = "ssm-test-vpc"
+  name    = "${var.app_name}-vpc"
   cidr    = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.names
