475db0114e7e01999e8c2a716840fd87a10cd30e
u1aryz <u1aryz.d@gmail.com>
2023-10-04 17:34:01 +0900

    Add RDS resources for PostgreSQL cluster

--- rds.tf
+++ rds.tf
@@ -15,3 +15,37 @@ resource "aws_security_group" "postgres_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "${var.app_name}-db-subnet-group"
+  subnet_ids = module.vpc.private_subnets
+}
+
+resource "aws_rds_cluster" "postgres" {
+  cluster_identifier = "${var.app_name}-postgres"
+  engine             = "aurora-postgresql"
+  engine_mode        = "provisioned"
+  engine_version     = "14.6"
+  availability_zones = data.aws_availability_zones.available.names
+  database_name      = "sample_db"
+
+  # Use Secrets Manager
+  master_username             = "sample_user"
+  manage_master_user_password = true
+
+  vpc_security_group_ids = [aws_security_group.postgres_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+
+  # destroyですぐ消せるようにする
+  deletion_protection = false
+  skip_final_snapshot = true
+}
+
+resource "aws_rds_cluster_instance" "postgres_instance" {
+  count              = 1
+  identifier         = "${var.app_name}-postgres-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.postgres.cluster_identifier
+  instance_class     = "db.t3.medium"
+  engine             = "aurora-postgresql"
+  engine_version     = "14.6"
+}
