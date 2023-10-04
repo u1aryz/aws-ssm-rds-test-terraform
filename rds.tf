52961bddbf6abaf2aacb2ed0a7f62f90d532d7aa
u1aryz <u1aryz.d@gmail.com>
2023-10-04 16:04:31 +0900

    Add AWS security group for postgres

---
+++ rds.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "postgres_sg" {
+  name        = "${var.app_name}-postgres-sg"
+  description = "Allow postgres access"
+  vpc_id      = module.vpc.vpc_id
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = module.vpc.private_subnets_cidr_blocks
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
