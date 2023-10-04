4f6f330bcd39aa5002dc6d40f6b4bae8a3e62908
u1aryz <u1aryz.d@gmail.com>
2023-10-04 16:36:44 +0900

    Refactor sg rule

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,13 @@
 resource "aws_security_group" "ec2_sg" {
   name   = "${var.app_name}-ec2-sg"
   vpc_id = module.vpc.vpc_id
-}
 
-resource "aws_security_group_rule" "ec2_egress_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_sg.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 data "aws_ami" "amazon2_amd64" {
